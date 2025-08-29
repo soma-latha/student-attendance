4e21edf31e49f91d81b4995975ffc7000388bda8
soma-latha <2317030@nec.edu.in>
2025-08-29 12:37:03 +0530

    Create SimpleDemo.java

---
+++ SimpleDemo.java
@@ -0,0 +1,12 @@
+class SimpleDemo {
+    public static void main(String[] args) {
+        // Print message
+        System.out.println("Hello, Welcome to Java!");
+
+        // Simple addition
+        int a = 10, b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
